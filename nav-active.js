b00a1beff93de98c1edb0925afd3419d3039bb2e
Sowjanya Chandramouli <sowjanya.srinivas1210@gmail.com>
2026-01-12 15:55:24 +0530

    Updated navbar and styles

--- nav-active.js
+++ nav-active.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+/* document.addEventListener("DOMContentLoaded", function () {
   let currentPage = window.location.pathname.split("/").pop();
 
   if (currentPage === "") {
@@ -9,5 +9,46 @@ document.addEventListener("DOMContentLoaded", function () {
     if (link.getAttribute("href") === currentPage) {
       link.classList.add("active");   // ✅ THIS is the key line
     }
+  }); */
+
+  
+
+  /* making the changes for nav-bar to be active all over the page */
+  document.addEventListener("DOMContentLoaded", function() {
+
+  const navbar = document.querySelector(".navbar");
+  const banner = document.querySelector(".brandrow");
+  const body = document.body;
+
+  let navbarHeight = navbar.offsetHeight;
+  const bannerHeight = banner.offsetHeight;
+
+  window.addEventListener("resize", () => {
+    navbarHeight = navbar.offsetHeight;
   });
+
+  window.addEventListener("scroll", function() {
+
+    if (window.scrollY >= bannerHeight) {
+      navbar.classList.add("sticky");
+      body.style.paddingTop = navbarHeight + "px";
+    } 
+    else {
+      navbar.classList.remove("sticky");
+      body.style.paddingTop = "0";
+    }
+
+  });
+
+  /* Highlight active page */
+  const links = document.querySelectorAll(".navbar .nav a");
+  const currentPage = window.location.pathname.split("/").pop();
+
+  links.forEach(link => {
+    if (link.getAttribute("href") === currentPage) {
+      link.classList.add("active");
+    }
+  });
+
 });
+
